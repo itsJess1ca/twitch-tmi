1f61501421ca93256f5c29bf6536e11ba93ead14
Jordan Ide <jorrdandied@hotmail.com>
2017-09-01 21:15:13 +0100

    fix(moderation): add moderation commands into commands bucket

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,5 +2,6 @@ export * from './followers-only';
 export * from './part';
 export * from './say';
 export * from './say-action';
+export * from './moderation';
 
 // TODO: Add the rest of the commands
